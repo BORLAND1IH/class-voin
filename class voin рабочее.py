ffcb4490dd804d5b2c61ce5a55c9836e90380797
Хвостов Иван Евгеньевич <158326488+BORLAND1IH@users.noreply.github.com>
2024-03-11 20:19:34 +0300

    Update class voin рабочее.py

--- class voin рабочее.py
+++ class voin рабочее.py
@@ -1,31 +1,32 @@
 import time #Команда записи Интервала в начале.
-print("Крестоносцы вновь нападают на Русские земли. Сможет ли Варяжское ополчение отбить атаку.")
-time.sleep(5) #Команда интервала между выводом, пишется здесь после принта с описанием.
+from random import randint as rnd
+
 class Воин:
     def __init__(self, name, hp, damage):
         self.name = name #str
         self.hp = hp #int
         self.damage = damage #int
-    def hit(self, Юнит):
-        Юнит.hp -= self.damage
-        if Юнит.hp > 0:
-            print(f'"{self.name}" атаковал "{Юнит.name}". У "{Юнит.name}" осталось {Юнит.hp} здоровья')
+    def hit(self, Unit):
+        Unit.hp -= self.damage
+        if Unit.hp > 0:
+            print(f'"{self.name}" атаковал "{Unit.name}". У "{Unit.name}" осталось {Unit.hp} здоровья')
         else:
-            print(f'"{self.name}" атаковал "{Юнит.name}". "{Юнит.name}" убит')
-            Юнит.hp = 0
-        return Юнит.hp
+            print(f'"{self.name}" атаковал "{Unit.name}". "{Unit.name}" убит')
+            Unit.hp = 0
+        return Unit.hp
  
-from random import randint as rnd
- 
-Юнит1 = Воин('Варяг', 100, 20)
-Юнит2 = Воин('Крестоносец', 100, 20)
-Юниты = [Юнит1, Юнит2]
- 
+Unit1 = Воин('Варяг', 100, 20)
+Unit2 = Воин('Крестоносец', 100, 20)
+Units = [Unit1, Unit2]
+
+print("Крестоносцы вновь нападают на Русские земли. Сможет ли Варяжское ополчение отбить атаку.")
+time.sleep(5) #Команда интервала между выводом, пишется здесь после принта с описанием.
+
 while True:
     attack_index = rnd(0,1) #Кто атакует
     target_index = (attack_index +1)%2 #Кого атакует
-    target_hp = Юниты[attack_index].hit(Юниты[target_index])
+    target_hp = Units[attack_index].hit(Units[target_index])
     time.sleep(3) #Команда интервала между выводом, пишется здесь после индекса.
     if target_hp == 0:
-        print(f'"{Юниты[attack_index].name}" Одержал Победу!')
+        print(f'"{Units[attack_index].name}" Одержал Победу!')
         break
